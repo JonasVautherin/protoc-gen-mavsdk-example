4f78aac2560403066519273d5cc84b9d0993042e
Jonas Vautherin <jonas.vautherin@gmail.com>
2022-06-27 00:48:59 +0200

    Add stream support to py

--- py/src/some_api_ext.cpp
+++ py/src/some_api_ext.cpp
@@ -1,5 +1,6 @@
 #include <example_library/some_api.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
 
 namespace py = pybind11;
 
@@ -12,6 +13,13 @@ PYBIND11_MODULE(example_library_py, m) {
         .def("poke", &SomeApi::poke, "Call a function (that may have side effects)")
         .def("echo", &SomeApi::echo, "Send a value to be echoed back")
         .def("add", &SomeApi::add, "Add two numbers and return the result")
+        .def("mode", &SomeApi::subscribe_mode, "Subscribe to 'mode' updates.");
+
+    py::enum_<SomeApi::Mode> mode(some_api, "Mode");
+    mode
+        .value("Unknown", SomeApi::Mode::Unknown)
+        .value("Idle", SomeApi::Mode::Idle)
+        .value("Active", SomeApi::Mode::Active);
 
     py::class_<SomeApi::TwoIntegers> two_integers(some_api, "TwoIntegers");
     two_integers
